4354578bc29c36674406a4375a6185ca0aebd553
Bruno Pissinato <pissinato.bruno@outlook.com>
2019-09-21 20:17:54 -0300

    First Commit

---
+++ Assets/Scripts/Desafio1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Desafio1 : MonoBehaviour
+{
+    public TMPro.TMP_InputField[] antonimos;
+    public TMPro.TMP_Text[] campos;
+    public TMPro.TMP_Text acertos;
+    
+
+    public void Comparacao()
+    {
+
+        Debug.Log(antonimos[1].text);
+        Debug.Log(campos[1]);
+
+        /* for (int i = 0; i <= 6; i++)
+        {
+            if (antonimos[i] == campos[i])
+            {
+
+            }
+        }*/
+        
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/PrimeiroDesafio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PrimeiroDesafio : MonoBehaviour
+{
+
+    public TMPro.TMP_InputField[] antonimos;
+    private string[] campos;
+    public Text acertos;
+    private int contador;
+    public Text corretor;
+
+
+
+    public void Comparacao()
+    {
+
+    campos = new string[6] { "bonito", "infeliz", "gordo", "mau", "calor", "triste" };
+
+        for (int i = 0; i <= 5; i++)
+        {
+            if (antonimos[i].text.ToUpper() == campos[i].ToUpper())
+            {
+                contador++;
+            }
+            else
+            {
+                corretor.text = antonimos[i].text.ToString();
+            }
+
+            SceneManager.LoadScene(0);
+        }
+
+        acertos.text = contador.ToString();
+
+
+    }
+
+
+}
